3c7402b6ffa54d570861971b2a89c93ccb070258
applandsys <applandsys@gmail.com>
2023-01-29 23:26:56 +0600

    ha roulette kaj korse akhon eta nia kaj korte hobe

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Button, StyleSheet, Text, View,  Animated,
+import {Button, StyleSheet, Text,TextInput , View,  Animated,
   Easing,
   TouchableHighlight
 } from 'react-native';
@@ -11,10 +11,17 @@ import {AuthContext} from '../context/AuthContext';
 
 const HomeScreen = () => {
   const {userInfo, isLoading, logout} = useContext(AuthContext);
-  const [count,setCount] = useState(0);
+  const [count,setCount] = useState(1);
+  const [betamount,setBetamount] = useState();
   const [stop,setStop] = useState(true);
+  const startdeg = Math.floor(Math.random() * 10) + 1;
+  const enddegree = Math.floor(Math.random() * 700) + 1;
+  const [degreestart,SetDegreestart] = useState(`${startdeg}deg`);
+  const [degreeend,SetDegreeend] = useState(`${enddegree}deg`);
 
   const myvalue = {rotateValueHolder: new Animated.Value(count)};
+
+  const roulette = require('../../assets/images/roulette.png');
 
  const startImageRotateFunction = () => {
    Animated.timing(myvalue.rotateValueHolder, {
@@ -23,32 +30,49 @@ const HomeScreen = () => {
       easing: Easing.linear,
       useNativeDriver: false,
     }).start();
-
-   // setCount(count + 1);
   }
 
+  const startRoulette = () =>{
+    setCount(count+1);
+  }
 
   const stopAnimation = () =>{
     myvalue.rotateValueHolder.stopAnimation();
   }
-
  
   useEffect(()=>{
     console.log(userInfo);
   })
 
 
-
   useEffect(() => {
+    startImageRotateFunction();
     console.log(count);
   }, [count]); // Only re-run the effect if count changes
 
   return (
     <View style={styles.container}>
       <Spinner visible={isLoading} />
-      <View><Text style={styles.welcome}>Welcome Shompa</Text></View>
+      <View><Text style={styles.welcome}>Welcome to Lotto</Text></View>
+    <View>
+
 
     <View>
+        <View>
+          <TextInput
+            style={styles.input} 
+            value={betamount}
+            placeholder="Enter Bet Amount"
+            onChangeText={text => setBetamount(text)}
+          />
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10}}>
+           <View><Text style={styles.betname}>BMS</Text></View>
+           <View><Text style={styles.betname}>Lamborgini</Text></View>
+           <View><Text style={styles.betname}>Nissan</Text></View>
+        </View>
+    </View>
+
     <Animated.Image
             style={{
               width: 200,
@@ -60,19 +84,19 @@ const HomeScreen = () => {
                     rotate: myvalue.rotateValueHolder.interpolate(
                         {
                           inputRange: [0, 1],
-                          outputRange: ['0deg', '360deg'],
+                          outputRange: [degreestart, degreeend],
                         }
                       )
                   }
                 ],
             }}
-              source={{uri:'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',}}
+              source={roulette}
           />
 
     </View>
 
       <View>
-      <Button title="Spin me" color="green" onPress={startImageRotateFunction} />
+        <Button title="Spin me" color="green" onPress={()=>{startRoulette()}} />
       </View>
 
       <View>
@@ -94,7 +118,23 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 18,
     marginBottom: 8,
+    color: 'red'
   },
+  input: {
+
+    borderColor: '#bbb',
+    borderRadius: 5,
+    paddingHorizontal: 14,
+    height: 50,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: '#ddd'
+
+  },
+  betname:{
+    color: 'green',
+    fontWeight: 'bold'
+  }
 });
 
 export default HomeScreen;
